a8b7d5233c52c5c5a5b3ab543d81e8caab15d07e
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-06-23 20:26:09 -0400

    cut out nice region

--- stack.py
+++ stack.py
@@ -9,27 +9,46 @@ from orphics.tools.stats import bin2D
 from orphics.tools.io import Plotter,dictFromSection,listFromConfig
 from orphics.analysis import flatMaps as fmaps
 import numpy as np
+import sys, os
+from skimage.transform import resize
 
+# For Mat on BNL
 map_location = "/astro/astronfs01/workarea/msyriac/act/cmb_maps/c7v5_release/s2/ACTPol_148_D56_PA1_S2_1way_I_src_free.fits"
 cat_location = "/astro/astronfs01/workarea/msyriac/act/ACTPol.fits"
+out_dir = os.environ['WWW']
 
+# For Teva
 #map_location = '../ACTdata/ACTPol_148_D56_PA1_S2_1way_I.fits'
 #cat_location = '../ACTdata/ACTPol.fits'
+#out_dir = "./"
 
 hdulist=fits.open(cat_location)
 catalog= hdulist[1].data
 lmap=liteMap.liteMapFromFits(map_location)
+#io.highResPlot2d(lmap.data,out_dir+"bigmap.png")
 RAs=[]
 DECs=[]
 widthStampArcminute = 60.
 pixScale = 0.5
 Nrands = 1000
 
+lmap.info()
+
+# select non-noisy part by trial and error
+lmap = lmap.selectSubMap(348,41,-8.3,5.3)
+io.highResPlot2d(lmap.data,out_dir+"cutmap.png")
+lmap.info()
+#sys.exit()
+
 ra_range = [lmap.x1-360.,lmap.x0] 
 dec_range = [lmap.y0,lmap.y1]
 
 
-def stack_on_map(lite_map,width_stamp_arcminute,pix_scale,catalog=None,n_random_points=None,rand_ra_range=None,rand_dec_range=None):
+#lmap.data = lmap.data - lmap.data.mean()
+
+
+
+def stack_on_map(lite_map,width_stamp_arcminute,pix_scale,ra_range,dec_range,catalog=None,n_random_points=None):
         width_stamp_degrees = width_stamp_arcminute /60.
         Np = np.int(width_stamp_arcminute/pix_scale+0.5)
         pad = np.int(Np/2+0.5)
@@ -45,24 +64,26 @@ def stack_on_map(lite_map,width_stamp_arcminute,pix_scale,catalog=None,n_random_
                 random = False
         else:
                 assert n_random_points is not None
-                assert len(rand_ra_range)==2
-                assert len(rand_dec_range)==2
+                assert len(ra_range)==2
+                assert len(dec_range)==2
                 looprange = range(0,n_random_points)
                 random = True
         
 	for i in looprange:
 		if random:
-                        ra = np.random.uniform(*rand_ra_range)
-                        dec =np.random.uniform(*rand_dec_range)
+                        ra = np.random.uniform(*ra_range)
+                        dec =np.random.uniform(*dec_range)
 		else:
                         ra=catalog[i][1]
 			dec=catalog[i][2]
 		ix, iy = lmap.skyToPix(ra,dec)
 		if ix>=pad and ix<lmap.Nx-pad and iy>=pad and iy<lmap.Ny-pad:
 			print(i)
-			print(ra,dec)
+			#print(ra,dec)
 			smap = lmap.selectSubMap(ra-width_stamp_degrees/2.,ra+width_stamp_degrees/2.,dec-width_stamp_degrees/2.,dec+width_stamp_degrees/2.)
-                	cutout = zoom(smap.data.copy(),zoom=(float(Np)/smap.data.shape[0],float(Np)/smap.data.shape[1]))
+                        #print (smap.data.shape)
+                	#cutout = zoom(smap.data.copy(),zoom=(float(Np)/smap.data.shape[0],float(Np)/smap.data.shape[1]))
+                	cutout = resize(smap.data.copy(),output_shape=(Np,Np))
                 	#print (cutout.shape)
 			stack = stack + cutout
 			xMap,yMap,modRMap,xx,yy = fmaps.getRealAttributes(smap)
@@ -74,7 +95,7 @@ def stack_on_map(lite_map,width_stamp_arcminute,pix_scale,catalog=None,n_random_
 	#print(smap.data.shape)
 	print(stack)
 	print(N)
-	io.quickPlot2d(stack,"stackrandom.png")
+	io.quickPlot2d(stack,out_dir+"stackrandom.png")
 
 	dt = pix_scale
 	arcmax = 20.
@@ -84,10 +105,10 @@ def stack_on_map(lite_map,width_stamp_arcminute,pix_scale,catalog=None,n_random_
 	pl = Plotter(labelX='Distance from Center (arcminutes)',labelY='Temperature Fluctuation ($\mu K$)', ftsize=10)
 	pl.add(cents,recons)
 	pl._ax.axhline(y=0.,ls="--",alpha=0.5)
-	pl.done("randomprofiles.png")
+	pl.done(out_dir+"randomprofiles.png")
         return stack, cents, recons
 
-#stack, cents, recons = stack_on_map(lmap,widthStampArcminute,pixScale,n_random_points=Nrands,rand_ra_range=ra_range,rand_dec_range=dec_range)
-stack, cents, recons = stack_on_map(lmap,widthStampArcminute,pixScale,catalog=catalog)
+#stack, cents, recons = stack_on_map(lmap,widthStampArcminute,pixScale,ra_range=ra_range,dec_range=dec_range,n_random_points=Nrands)
+stack, cents, recons = stack_on_map(lmap,widthStampArcminute,pixScale,ra_range=ra_range,dec_range=dec_range,catalog=catalog)
 
 
